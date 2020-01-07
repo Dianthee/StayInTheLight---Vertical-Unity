3d43c6e769b3c9925cbe6d077a43bdf9792fa809
Diana Martin <dianamartin.cuadern@gmail.com>
2020-01-07 23:19:08 +0100

    Unity Vertical Project

---
+++ StayInTheLight_DianaMartin/Assets/Scripts/Triggers.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Triggers : MonoBehaviour
+{
+
+    public GameObject[] Save;
+    public GameObject counter;
+
+    public float timeVal;
+
+    public Text textoVictoria;
+    public Text textoDerrota;
+    public Text tiempoPartida;
+
+    bool notSave;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if(notSave){
+            counter.SetActive(true);
+            Timer();
+        }else{
+
+        }
+
+    }
+
+    void Timer(){
+
+        tiempoPartida.text=timeVal.ToString("00");
+        timeVal -= Time.deltaTime;
+
+        if(timeVal<= 0){
+             timeVal = 0;
+         }
+
+        if(timeVal<=0.0f){
+            Debug.Log("Tiempo a 0");
+             //scriptBola.GetComponent<BALLCONTROLLER>().ballSpeed = 0;
+             textoDerrota.gameObject.SetActive(true);
+        }
+
+
+    }
+
+    void OnTriggerEnter(Collider Save) {
+                
+        switch (Save.tag)
+        {
+            case "YouAreSaveHere":
+                Debug.Log("Now you are on a safe place!");
+                counter.SetActive(false);
+                notSave = false;
+                timeVal=5f;
+                tiempoPartida.text=timeVal.ToString("00");
+            break;
+        }
+    }
+
+    void OnTriggerExit(Collider Save) {
+
+        notSave = true;
+        Debug.Log("You aren't safe here!");
+    }
+}
