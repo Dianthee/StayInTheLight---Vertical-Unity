d8d2f749700663d4fdb7a857b9ccc1afb767a98d
Diana Martin <dianamartin.cuadern@gmail.com>
2020-01-08 22:47:21 +0100

    Programmed Random Exit + Debug OpenDoor

---
+++ StayInTheLight_DianaMartin/Assets/Door.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject exit;
+    
+    [SerializeField]
+    private GameObject door;
+
+    private bool isOpen;
+
+    public void ActiveDoor(){
+        exit.SetActive(true);
+    }
+
+    IEnumerator  OpenDoor(){
+
+        Debug.Log("Opening door");
+        while (true)
+        {
+            yield return null;
+        }
+
+    }
+
+    private void OnTriggerStay(Collider other) {
+        if(other.tag == "Player")
+            if (FindObjectOfType<Triggers>().haveKey && Input.GetKeyDown(KeyCode.F))
+                if (!isOpen)
+                {
+                    StartCoroutine(OpenDoor());
+                    isOpen = true;
+                }
+    }
+}
---
+++ StayInTheLight_DianaMartin/Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    Door[] doors;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        doors = FindObjectsOfType<Door>();
+
+        int indexDoorActive = Random.Range(0, doors.Length);
+        doors[indexDoorActive].ActiveDoor();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
