ea270e0b7590267713f1bc6b9e9942eb668e3993
Diana <diana.martin@hotmail.es>
2020-01-09 08:59:25 +0100

    Added Scene Main Menu + Title

---
+++ StayInTheLight_DianaMartin/Assets/Scripts/Menu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+
+	public void NewGame(){
+	
+		SceneManager.LoadScene (2);
+	}
+
+	 public void OpenURL()
+      {
+          //Application.OpenURL("https://twitter.com/weasel_games_st");
+      }
+
+	public void Quit(){
+	
+		Application.Quit ();
+
+	}
+
+}
